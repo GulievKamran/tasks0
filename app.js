2b42ec18f0303d62fee311d51142c7c96d7f0e15
GulievKamran <kamranquliyev7778@gmail.com>
2025-04-10 13:44:08 -0700

    if,switch

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const a=Number(prompt("your age"));
-const b=Number(prompt("your height"));
-if(isNaN(a) || isNaN(b)){
-    alert("Please enter numeric values only")
-const outcome= a*b;
-document.getElementById("result").innerText(`${a}*${b}=${outcome}`);}
+const n1=+prompt("birinci reqem yaz");
+const o=prompt("operatorlardan birini yaz(+,-,/,*)");
+const n2=+prompt("ikinci reqemi daxil et");
+
+if(o==="+"){
+    document.getElementById("result").innerText(`${n1}+${n2}=${n1 + n2}`);
+} else if(o==="-"){
+    document.getElementById("result").innerText(`${n1}-${n2}=${n1-n2}`);
+} else if (0==="/"){
+    document.getElementById("result").innerText(`${n1}/${n2}=${n1/n2}`);
+}else if(0==="*") {
+    document.getElementById("result").innerText(`${n1}/${n2}=${n1*n2}`);
+}
+else { document.getElementById("result").innerText("Diqqetli oxuyun!!!!!!!!!!!");}
