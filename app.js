6ffe97b419f30b2ce7bf39b0e97961d6b20a3809
GulievKamran <kamranquliyev7778@gmail.com>
2025-04-10 15:04:42 -0700

    if witch ready

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const o=prompt("operatorlardan birini yaz(+,-,/,*)");
 const n2=+prompt("ikinci reqemi daxil et");
 
 if(o==="+"){
-    document.getElementById("result").innerText(`${n1}+${n2}=${n1 + n2}`);
+    document.getElementById("outcome").innerText=`${n1}+${n2}=${n1 + n2}`;
 } else if(o==="-"){
-    document.getElementById("result").innerText(`${n1}-${n2}=${n1-n2}`);
-} else if (0==="/"){
-    document.getElementById("result").innerText(`${n1}/${n2}=${n1/n2}`);
-}else if(0==="*") {
-    document.getElementById("result").innerText(`${n1}/${n2}=${n1*n2}`);
+    document.getElementById("outcome").innerText=`${n1}-${n2}=${n1-n2}`;
+} else if (o==="/"){
+    document.getElementById("outcome").innerText=`${n1}/${n2}=${n1/n2}`;
+}else if(o==="*") {
+    document.getElementById("outcome").innerText=`${n1}*${n2}=${n1*n2}`;
 }
-else { document.getElementById("result").innerText("Diqqetli oxuyun!!!!!!!!!!!");}
+else { document.getElementById("outcome").innerText="Diqqetli oxuyun!!!!!!!!!";}
